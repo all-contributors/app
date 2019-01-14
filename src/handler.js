09d83466b2f42831cfad28c71e19f32d9706063c
Jake Bolam <jake.bolam@gmail.com>
2019-01-14 23:30:52 +0800

    feat: add sentry support (#23)

--- src/handler.js
+++ src/handler.js
@@ -1,3 +1,5 @@
-const { serverless } = require('@probot/serverless-lambda')
+// TODO: put back on release of: https://github.com/probot/serverless-lambda/pull/13/files
+// const { serverless } = require('@probot/serverless-lambda')
+const { serverless } = require('serverless-lambda')
 const appFn = require('./')
 module.exports.probot = serverless(appFn)
