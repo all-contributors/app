c0338fcf9ea6717359f1bdb19fd5b69a72156aa8
Jake Bolam <jake.bolam@gmail.com>
2019-01-15 00:28:38 +0800

    fix: ensure path imports correctly

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,5 @@
 // TODO: put back on release of: https://github.com/probot/serverless-lambda/pull/13/files
 // const { serverless } = require('@probot/serverless-lambda')
-const { serverless } = require('serverless-lambda')
+const { serverless } = require('./serverless-lambda')
 const appFn = require('./')
 module.exports.probot = serverless(appFn)
