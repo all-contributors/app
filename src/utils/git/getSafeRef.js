8cd9ce75ae7930f0fe6c8e29ee07873df4d72612
Jake Bolam <jake.bolam@gmail.com>
2019-01-19 08:20:26 -0500

    fix: ensure usernames are valid

---
+++ src/utils/git/getSafeRef.js
@@ -0,0 +1,11 @@
+function getSafeRef(ref) {
+    // Replace fullstops
+    // ~, ^ or : ? * [
+    // /
+    // remove @ sybmobls
+    // remove backslash
+    const safeRef = ref.replace(/[\.\[\~\^\:\?\*\@\/\\]/gi, '-')
+    return safeRef
+}
+
+module.exports = getSafeRef
