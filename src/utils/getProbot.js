48e620e5167ea279910af131fbd6930fdab03373
Jake Bolam <jake.bolam@gmail.com>
2019-01-21 22:06:08 -0500

    fix: stop spamming messages

--- src/utils/getProbot.js
+++ src/utils/getProbot.js
@@ -1,16 +1,12 @@
 const { createProbot } = require('probot')
 const { findPrivateKey } = require('probot/lib/private-key')
 
-let probot
-
 function getProbot() {
-    probot =
-        probot ||
-        createProbot({
-            id: process.env.APP_ID,
-            secret: process.env.WEBHOOK_SECRET,
-            cert: findPrivateKey(),
-        })
+    const probot = createProbot({
+        id: process.env.APP_ID,
+        secret: process.env.WEBHOOK_SECRET,
+        cert: findPrivateKey(),
+    })
 
     if (process.env.SENTRY_DSN) {
         probot.load(require('probot/lib/apps/sentry'))
