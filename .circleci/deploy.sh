a2d2e9ecd26efd0b36e1ecfac55a6cc4bb8c0a6a
Jake Bolam <jake.bolam@gmail.com>
2019-01-26 17:12:06 -0500

    infra: link to sandbox

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -6,6 +6,7 @@ fi
 
 if [[ ${STAGE} == 'sandbox' ]]; then
     echo 'Using sandbox secrets'
+    echo 'You can test the sandbox over on: https://github.com/all-contributors-sandbox/test-all-contributors'
     export WEBHOOK_SECRET=${SANDBOX_WEBHOOK_SECRET}
     export PRIVATE_KEY=${SANDBOX_PRIVATE_KEY}
 else
