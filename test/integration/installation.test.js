ea3f5c38855a7f0ecf2d00824432e396ec1e7480
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2020-12-31 15:12:47 -0800

    test: 100% coverage for installation tests

--- test/integration/installation.test.js
+++ test/integration/installation.test.js
@@ -7,6 +7,9 @@ const app = require("../../app");
 
 // fixtures
 const installationCreated = require("../fixtures/installation.created.json");
+const installationDeleted = require("../fixtures/installation.deleted.json");
+const installationRepositoriesAdded = require("../fixtures/installation_repositories.added.json");
+const installationSuspend = require("../fixtures/installation.suspend.json");
 
 let output = [];
 const streamLogsToOutput = new Stream.Writable({ objectMode: true });
@@ -39,6 +42,33 @@ describe("installation event", () => {
     await probot.receive({
       name: "installation",
       payload: installationCreated,
+    });
+
+    expect(output).toMatchSnapshot();
+  });
+
+  test("installation.deleted", async () => {
+    await probot.receive({
+      name: "installation",
+      payload: installationDeleted,
+    });
+
+    expect(output).toMatchSnapshot();
+  });
+
+  test("installation_repositories.added", async () => {
+    await probot.receive({
+      name: "installation_repositories",
+      payload: installationRepositoriesAdded,
+    });
+
+    expect(output).toMatchSnapshot();
+  });
+
+  test("installation.suspend", async () => {
+    await probot.receive({
+      name: "installation",
+      payload: installationSuspend,
     });
 
     expect(output).toMatchSnapshot();
