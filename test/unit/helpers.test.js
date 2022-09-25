629c5b82bcb7de407ff6e7d72272013b0f743a24
Angel Aviel Domaoan <13580338+tenshiAMD@users.noreply.github.com>
2022-09-25 08:32:16 +0800

    fix: add stricter checks in link for `profile` (#422)
    Co-authored-by: Roshan Jossy <roshanjossey@gmail.com>

--- test/unit/helpers.test.js
+++ test/unit/helpers.test.js
@@ -1,19 +1,75 @@
-const { generateValidLink } = require('../../lib/modules/helpers');
+const { generateValidProfileLink } = require('../../lib/modules/helpers');
 
-describe('generateValidLink', () => {
-  const username = 'tenshiAMD'
+describe('generateValidProfileLink', () => {
+  const githubProfileUrl = 'https://github.com/tenshiAMD'
 
-  test('return valid link - no protocol', async () => {
-    let url = 'tenshiamd.com';
-    let validUrl = generateValidLink(url, username);
+  test('returns valid link - valid URL format having `https` protocol', async () => {
+    let url = 'https://tenshiamd.com';
+    let validUrl = generateValidProfileLink(url, githubProfileUrl);
 
-    expect(validUrl).toEqual(`http://${url}/`);
+    expect(validUrl).toEqual(url);
   });
 
-  test('return valid link - incomplete URL format', async () => {
-    let url = 'contributor';
-    let validUrl = generateValidLink(url, username);
+  test('returns valid link - valid URL format having `http` protocol', async () => {
+    let url = 'http://tenshiamd.com';
+    let validUrl = generateValidProfileLink(url, githubProfileUrl);
 
-    expect(validUrl).toEqual(`https://github.com/${username}/`);
+    expect(validUrl).toEqual(url);
+  });
+
+  test('returns valid link - valid URL format with `null` githubProfileUrl', async () => {
+    let url = 'https://tenshiamd.com';
+    let validUrl = generateValidProfileLink(url, null);
+
+    expect(validUrl).toEqual(url);
+  });
+
+  test('returns valid link - valid URL format with `http` in between', async () => {
+    let url = 'tenshhttpiamd.com';
+    let validUrl = generateValidProfileLink(url, githubProfileUrl);
+
+    expect(validUrl).toEqual(url);
+  });
+
+  test('returns valid link - valid URL format with `https` in between', async () => {
+    let url = 'tenshhttpsiamd.com';
+    let validUrl = generateValidProfileLink(url, githubProfileUrl);
+
+    expect(validUrl).toEqual(url);
+  });
+
+  test('returns valid link - no protocol', async () => {
+    let url = 'tenshiamd.com';
+    let validUrl = generateValidProfileLink(url, githubProfileUrl);
+
+    expect(validUrl).toEqual(url);
+  });
+
+  test('returns valid link - no protocol and starting with `http`', async () => {
+    let url = 'httptenshiamd.com';
+    let validUrl = generateValidProfileLink(url, githubProfileUrl);
+
+    expect(validUrl).toEqual(url);
+  });
+
+  test('returns valid link - no protocol and starting with `https`', async () => {
+    let url = 'httpstenshiamd.com';
+    let validUrl = generateValidProfileLink(url, githubProfileUrl);
+
+    expect(validUrl).toEqual(url);
+  });
+
+  test('returns valid link - incomplete URL format', async () => {
+    let url = 'contributor';
+    let validUrl = generateValidProfileLink(url, githubProfileUrl);
+
+    expect(validUrl).toEqual(githubProfileUrl);
+  });
+
+  test('returns valid link - incomplete URL format with `null` githubProfileUrl', async () => {
+    let url = 'contributor';
+    let validUrl = generateValidProfileLink(url, null);
+
+    expect(validUrl).toEqual('');
   });
 });
