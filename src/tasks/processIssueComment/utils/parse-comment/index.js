f8adcf9795573912b2cf6adef3345c3a328e4718
Jake Bolam <jake.bolam@gmail.com>
2022-10-06 15:02:01 -0400

    feat: support adding multiple users in a single message (#127)
    Co-authored-by: Angel Aviel Domaoan <13580338+tenshiAMD@users.noreply.github.com>

---
+++ src/tasks/processIssueComment/utils/parse-comment/index.js
@@ -0,0 +1,254 @@
+const nlp = require('compromise')
+
+// Types that are valid (multi words must all be lower case)
+const validContributionTypes = [
+    'blog',
+    'bug',
+    'business',
+    'code',
+    'content',
+    'design',
+    'doc',
+    'eventorganizing',
+    'example',
+    'financial',
+    'fundingfinding',
+    'ideas',
+    'infra',
+    'maintenance',
+    'platform',
+    'plugin',
+    'projectManagement',
+    'question',
+    'review',
+    'security',
+    'talk',
+    'test',
+    'tool',
+    'translation',
+    'tutorial',
+    'usertesting',
+    'video',
+]
+
+// Types that are valid multi words, that we need to re map back to there camelCase parts
+const validMultiContributionTypesMapping = {
+    eventorganizing: 'eventOrganizing',
+    fundingfinding: 'fundingFinding',
+    usertesting: 'userTesting',
+    projectmanagement: 'projectManagement',
+}
+
+// Additional terms to match to types (plurals, aliases etc)
+const contributionTypeMappings = {
+    blogs: 'blog',
+    blogging: 'blog',
+    bugs: 'bug',
+    codes: 'code',
+    coding: 'code',
+    designing: 'design',
+    desigs: 'design',
+    doc: 'doc',
+    docs: 'doc',
+    documenting: 'doc',
+    documentation: 'doc',
+    examples: 'example',
+    finance: 'financial',
+    financials: 'financial',
+    funds: 'fundingfinding',
+    idea: 'ideas',
+    infras: 'infra',
+    infrastructure: 'infra',
+    maintaining: 'maintenance',
+    management: 'projectManagement',
+    managing: 'projectManagement',
+    platforms: 'platform',
+    plugins: 'plugin',
+    project: 'projectManagement',
+    projectManaging: 'projectManagement',
+    questions: 'question',
+    reviews: 'review',
+    securing: 'security',
+    talks: 'talk',
+    tests: 'test',
+    testing: 'test',
+    tools: 'tool',
+    tooling: 'tool',
+    translator: 'translation',
+    translating: 'translation',
+    translations: 'translation',
+    tutorials: 'tutorial',
+    videoes: 'video',
+}
+
+// Additional terms to match to types (plurals, aliases etc) that are multi word
+const contributionTypeMultiWordMapping = {
+    'event organizing': 'eventOrganizing',
+    'fund finding': 'fundingFinding',
+    'funding finding': 'fundingFinding',
+    'user testing': 'userTesting',
+    'business development': 'business',
+    'project management': 'projectManagement',
+}
+
+const Contributions = {}
+
+validContributionTypes.forEach(type => {
+    Contributions[type] = 'Contribution'
+})
+
+Object.keys(contributionTypeMappings).forEach(type => {
+    Contributions[`${type}`] = 'Contribution'
+})
+
+const plugin = {
+    words: {
+        ...Contributions,
+        add: 'Action',
+    },
+}
+
+nlp.plugin(plugin)
+
+function findWho(message, action) {
+    function findWhoSafe(match) {
+        message = message.replace('-', '#/#') // workaround (https://github.com/spencermountain/compromise/issues/726)
+        const whoNormalizeSettings = {
+            whitespace: true, // remove hyphens, newlines, and force one space between words
+            case: false, // keep only first-word, and 'entity' titlecasing
+            numbers: false, // turn 'seven' to '7'
+            punctuation: true, // remove commas, semicolons - but keep sentence-ending punctuation
+            unicode: false, // visually romanize/anglicize 'Björk' into 'Bjork'.
+            contractions: false, // turn "isn't" to "is not"
+            acronyms: false, //remove periods from acronyms, like 'F.B.I.'
+            parentheses: false, //remove words inside brackets (like these)
+            possessives: false, // turn "Google's tax return" to "Google tax return"
+            plurals: false, // turn "batmobiles" into "batmobile"
+            verbs: false, // turn all verbs into Infinitive form - "I walked" → "I walk"
+            honorifics: false, //turn 'Vice Admiral John Smith' to 'John Smith'
+        }
+        const matchedSet = nlp(message)
+            .match(match)
+            .normalize(whoNormalizeSettings)
+            .data()
+
+        if (matchedSet.length > 0) {
+            matchedText = matchedSet[0].text
+            matchedText = matchedText.replace('#/#', '-') 
+
+            return matchedText 
+        }
+    }
+
+    const whoMatchedMention = findWhoSafe(`@[.]`)
+    // TODO: Unused
+    // if (whoMatchedMention) {
+    //     return whoMatchedMention
+    // }
+
+    const whoMatchedByAction = findWhoSafe(`${action} [.]`)
+    if (whoMatchedByAction) {
+        return whoMatchedByAction
+    }
+
+    const whoMatchedByFor = findWhoSafe(`[.] for`)
+    if (whoMatchedByFor) {
+        return whoMatchedByFor
+    }
+}
+
+function parseAddSentence(message, action) {
+    message = message.split(',').map(e => e.trim()).join(', ')
+
+    const whoMatched = findWho(message, action)
+    if (!whoMatched) {
+        return {
+            who: undefined,
+        }
+    }
+
+    const who = whoMatched.startsWith('@') ? whoMatched.substr(1) : whoMatched
+
+    // Contributions
+    const doc = nlp(message).toLowerCase()
+    // This is to support multi word 'matches' (altho the compromise docs say it supports this *confused*)
+    Object.entries(contributionTypeMultiWordMapping).forEach(
+        ([multiWordType, singleWordType]) => {
+            doc.replace(multiWordType, singleWordType)
+        },
+    )
+
+    const rawContributions = doc
+        .match('#Contribution')
+        .data()
+        .map(data => {
+            // This removes whitespace, commas etc
+            return data.normal
+        })
+
+    const contributions = 
+        doc
+        .match('#Contribution')
+        .data()
+        .map(data => {
+            // This removes whitespace, commas etc
+            return data.normal
+        }).filter((element, index) => {
+            return rawContributions.indexOf(element) === index;
+        }).map(type => {
+            if (contributionTypeMappings[type])
+                return contributionTypeMappings[type]
+            return type
+        })
+        .map(type => {
+            // Convert usertesting to userTesting for the node api
+            if (validMultiContributionTypesMapping[type])
+                return validMultiContributionTypesMapping[type]
+            return type
+        })
+    
+    return {
+        who,
+        contributions,
+    }
+}
+
+function parseAddComment(message, action) {
+    const contributors = {}
+
+    const sentences = nlp(message).sentences()
+    sentences.forEach(function(sentence) {
+        const sentenceRaw = sentence.data()[0].text
+        const { who, contributions } = parseAddSentence(sentenceRaw, action)
+
+        if (who) {
+            contributors[who] = contributions
+        }
+    })
+
+    return {
+        action: 'add',
+        contributors,
+    }
+}
+
+function parseComment(message) {
+    const doc = nlp(message)
+
+    const action = doc
+        .toLowerCase()
+        .match('#Action')
+        .normalize()
+        .out('string')
+
+    // TODO: Check each sentennce
+    if (action.match('add')) {
+        return parseAddComment(message, action)
+    }
+
+    return {
+        action: false,
+    }
+}
+
+module.exports = parseComment
---
+++ test/tasks/processIssueComment/utils/parse-comment/index.test.js
@@ -0,0 +1,258 @@
+const parseComment = require('../../../../../src/tasks/processIssueComment/utils/parse-comment')
+
+describe('parseComment', () => {
+    const testBotName = 'all-contributors'
+
+    test('Basic intent to add', () => {
+        expect(
+            parseComment(
+                `@${testBotName} please add jakebolam for doc, infra and code`,
+            ),
+        ).toEqual({
+            action: 'add',
+            contributors: {
+                jakebolam: ['doc', 'infra', 'code'],
+            },
+        })
+    })
+
+    test('Basic intent to add - ignore case (for action and contributions, NOT for user)', () => {
+        expect(
+            parseComment(
+                `@${testBotName} please Add jakeBolam for DOC, inFra and coDe`,
+            ),
+        ).toEqual({
+            action: 'add',
+            contributors: {
+                jakeBolam: ['doc', 'infra', 'code'],
+            },
+        })
+    })
+
+    test('Basic intent to add - non name username', () => {
+        expect(
+            parseComment(`@${testBotName} please add tbenning for design`),
+        ).toEqual({
+            action: 'add',
+            contributors: {
+                tbenning: ['design'],
+            },
+        })
+    })
+
+    test('Basic intent to add - captialized username', () => {
+        expect(
+            parseComment(`@${testBotName} please add Rbot25_RULES for tool`),
+        ).toEqual({
+            action: 'add',
+            contributors: {
+                Rbot25_RULES: ['tool'],
+            },
+        })
+    })
+
+    test('Basic intent to add - username with dash', () => {
+        expect(
+            parseComment(`@${testBotName} please add tenshi-AMD for tool`),
+        ).toEqual({
+            action: 'add',
+            contributors: {
+                "tenshi-AMD": ['tool'],
+            },
+        })
+    })
+
+    test('Basic intent to add - with plurals', () => {
+        expect(
+            parseComment(`@${testBotName} please add dat2 for docs`),
+        ).toEqual({
+            action: 'add',
+            contributors: {
+                dat2: ['doc'],
+            },
+        })
+    })
+
+    test('Add multiple when not spaced (just split via commas)', () => {
+        expect(
+            parseComment(
+                `@${testBotName} please add @stevoo24 for code,content`,
+            ),
+        ).toEqual({
+            action: 'add',
+            contributors: {
+                stevoo24: ['code', 'content'],
+            },
+        })
+    })
+
+    test(`Interpret users who's names are contributions`, () => {
+        expect(
+            parseComment(`@${testBotName} please add @ideas for ideas`),
+        ).toEqual({
+            action: 'add',
+            contributors: {
+                ideas: ['ideas'],
+            },
+        })
+    })
+
+    test('Support full words (like infrastructure)', () => {
+        expect(
+            parseComment(
+                `@${testBotName} please add jakebolam for infrastructure, documentation`,
+            ),
+        ).toEqual({
+            action: 'add',
+            contributors: {
+                jakebolam: ['infra', 'doc'],
+            },
+        })
+    })
+
+    test('Support adding people with mentions', () => {
+        expect(
+            parseComment(
+                `@${testBotName} please add @sinchang for infrastructure`,
+            ),
+        ).toEqual({
+            action: 'add',
+            contributors: {
+                sinchang: ['infra'],
+            },
+        })
+    })
+
+    test('Support alternative sentences', () => {
+        expect(
+            parseComment(`@${testBotName} add @sinchang for infrastructure`),
+        ).toEqual({
+            action: 'add',
+            contributors: {
+                sinchang: ['infra'],
+            },
+        })
+
+        expect(
+            parseComment(
+                `Jane you are crushing it in documentation and your infrastructure work has been great too, let's add jane.doe23 for her contributions. cc @${testBotName}`,
+            ),
+        ).toEqual({
+            action: 'add',
+            contributors: {
+                'jane.doe23': ['doc', 'infra'],
+            },
+        })
+    })
+
+    test('Support split words (like user testing)', () => {
+        expect(
+            parseComment(
+                `@${testBotName} please add jakebolam for infrastructure, fund finding`,
+            ),
+        ).toEqual({
+            action: 'add',
+            contributors: {
+                jakebolam: ['infra', 'fundingFinding'],
+            },
+        })
+
+        expect(
+            parseComment(
+                `@${testBotName} please add jakebolam for infrastructure, user testing and testing`,
+            ),
+        ).toEqual({
+            action: 'add',
+            contributors: {
+                jakebolam: ['infra', 'userTesting', 'test'],
+            },
+        })
+    })
+
+    test('Support split words types that are referenced via other terms (e.g. a plural split word)', () => {
+        expect(
+            parseComment(
+                `@${testBotName} please add @jakebolam for infrastructure, funds`,
+            ),
+        ).toEqual({
+            action: 'add',
+            contributors: {
+                jakebolam: ['infra', 'fundingFinding'],
+            },
+        })
+    })
+
+    // TODO: Looks like this is impossible to parse correctly, maybe we can change the format instead
+    // test('Add multiple users in 1 hit - for some contributions', () => {
+    //     expect(
+    //         parseComment(
+    //             `@${testBotName} please add @jakebolam and @tbenning for doc and review`,
+    //         ),
+    //     ).toEqual({
+    //         action: 'add',
+    //         contributors: {
+    //             jakebolam: ['doc', 'review'],
+    //             tbenning: ['doc', 'review'],
+    //         },
+    //     })
+    // })
+
+    test('Add multiple users in 1 hit - seperate sentences', () => {
+        expect(
+            parseComment(
+                `@${testBotName} add @kazydek for doc, review, maintenance. Please add akucharska for code, design. Please add derberg for infra and ideas`,
+            ),
+        ).toEqual({
+            action: 'add',
+            contributors: {
+                kazydek: ['doc', 'review', 'maintenance'],
+                akucharska: ['code', 'design'],
+                derberg: ['infra', 'ideas'],
+            },
+        })
+    })
+
+    // TODO: Looks like this is gramatically incorrect
+    // test('Add multiple users in 1 hit - sentences seperated by commas :think:', () => {
+    //     expect(
+    //         parseComment(
+    //             `@${testBotName} please add kazydek for doc, review, maintenance, please add akucharska for code, maintenance and please add derberg for doc, ideas`,
+    //         ),
+    //     ).toEqual({
+    //         action: 'add',
+    //         contributors: {
+    //             kazydek: ['doc', 'review', 'maintenance'],
+    //             akucharska: ['code', 'maintenance'],
+    //             derberg: ['doc', 'ideas']
+    //         },
+    //     })
+    // })
+
+    test('Add multiple users in 1 hit - from seperate lines', () => {
+        expect(
+            parseComment(
+                `
+                    @all-contributors 
+                    please add @mikeattara for ideas, infra and design
+                    please add @The24thDS for infra and review
+                    please add @tbenning for code
+                `
+            ),
+        ).toEqual({
+            action: 'add',
+            contributors: {
+                mikeattara: ['ideas', 'infra', 'design'],
+                The24thDS: ['infra', 'review'],
+                tbenning: ['code']
+            },
+        })
+    })
+
+    test('Intent unknown', () => {
+        expect(
+            parseComment(`@${testBotName} please lollmate for tool`),
+        ).toEqual({
+            action: false,
+        })
+    })
+})
