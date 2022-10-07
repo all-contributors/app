5821e4482b4cb62f517541398dca9047e0734222
Angel Aviel Domaoan <13580338+tenshiAMD@users.noreply.github.com>
2022-10-08 06:54:04 +0800

    chore: add snapshot for testing multiple contributors at single commit

--- test/integration/issue_comment.test.js
+++ test/integration/issue_comment.test.js
@@ -13,11 +13,15 @@ const issueCommentCreatedByAppPayload = require("../fixtures/issue_comment.creat
 const issueCommentCreatedNotForAppPayload = require("../fixtures/issue_comment.created-not-for-app.json");
 const issueCommentCreatedPayloadUnknownIntention = require("../fixtures/issue_commented.created.unknown-intention.json");
 const issueCommentCreatedPayloadUnknownContribution = require("../fixtures/issue_comment.created-unknown-contribution.json");
+const issueCommentCreatedMultipleContributorsPayload = require("../fixtures/issue_comment.created-multiple-contributors.json");
 const reposGetContentsAllContributorsRCdata = require("../fixtures/repos.getContents.all-contributorsrc.json");
+const reposGetContentsAllContributorsRCBlankContributorsdata = require("../fixtures/repos.getContents.all-contributorsrc-blank-contributors.json");
 const reposGetContentsAllContributorsRCdata26files = require("../fixtures/repos.getContents.all-contributorsrc-26-files.json");
 const reposGetContentsAllContributorsRCdataSkipCiFalse = require("../fixtures/repos.getContents.all-contributorsrc-skip-ci-false.json");
 const reposGetContentsAllContributorsRCdataInvalidSyntax = require("../fixtures/repos.getContents.all-contributorsrc-invalid-syntax.json");
+const usersGetByUsernameGr2mData = require("../fixtures/users.getByUsername.gr2m.json");
 const usersGetByUsernameJakeBolamdata = require("../fixtures/users.getByUsername.jakebolam.json");
+const usersGetByUsernameTenshiamdData = require("../fixtures/users.getByUsername.tenshiamd.json");
 const usersGetByUsernameNoBlogAndNameData = require("../fixtures/users.getByUsername.without-name-and-blog.json");
 const reposGetContentsREADMEMDdata = require("../fixtures/repos.getContents.README.md.json");
 const gitGetRefdata = require("../fixtures/git.getRef.json");
@@ -760,5 +764,87 @@ describe("issue_comment event", () => {
       id: "1",
       payload: issueCommentCreatedNotForAppPayload,
     });
+  });
+
+  test("Happy path, add correct new multiple contributors", async () => {
+    const mock = nock("https://api.github.com")
+      .get(
+        `/repos/all-contributors/all-contributors-bot/git/ref/heads%2Fall-contributors%2Fadd-gr2m`
+      )
+      .reply(404)
+
+      .get(
+        `/repos/all-contributors/all-contributors-bot/git/ref/heads%2Fall-contributors%2Fadd-tenshiamd`
+      )
+      .reply(404)
+
+      .get(
+        "/repos/all-contributors/all-contributors-bot/contents/.all-contributorsrc?ref=master"
+      )
+      .reply(200, reposGetContentsAllContributorsRCdata)
+
+      .get("/users/gr2m")
+      .reply(200, usersGetByUsernameGr2mData)
+      
+      .get("/users/tenshiamd")
+      .reply(200, usersGetByUsernameTenshiamdData)
+
+      .get(
+        "/repos/all-contributors/all-contributors-bot/contents/README.md?ref=master"
+      )
+      .reply(200, reposGetContentsREADMEMDdata)
+
+      .get(
+        `/repos/all-contributors/all-contributors-bot/git/ref/heads%2Fmaster`
+      )
+      .reply(200, gitGetRefdata)
+
+      .post(`/repos/all-contributors/all-contributors-bot/git/refs`, (body) => {
+        expect(body).toMatchSnapshot("request body");
+        return true;
+      })
+      .reply(201, gitCreateRefdata)
+
+      .put(
+        `/repos/all-contributors/all-contributors-bot/contents/.all-contributorsrc`,
+        (body) => {
+          expect(body).toMatchSnapshot("request body");
+          return true;
+        }
+      )
+      .reply(200, reposUpdateFiledata)
+
+      .put(
+        `/repos/all-contributors/all-contributors-bot/contents/README.md`,
+        (body) => {
+          expect(body).toMatchSnapshot("request body");
+          return true;
+        }
+      )
+      .reply(200, reposUpdateFiledata)
+
+      .post(`/repos/all-contributors/all-contributors-bot/pulls`, (body) => {
+        expect(body).toMatchSnapshot("request body");
+        return true;
+      })
+      .reply(201, pullsCreatedata)
+
+      .post(
+        "/repos/all-contributors/all-contributors-bot/issues/1/comments",
+        (body) => {
+          expect(body).toMatchSnapshot("request body");
+          return true;
+        }
+      )
+      .reply(200);
+
+    await probot.receive({
+      name: "issue_comment",
+      id: "1",
+      payload: issueCommentCreatedMultipleContributorsPayload,
+    });
+
+    expect(mock.activeMocks()).toStrictEqual([]);
+    expect(output).toMatchSnapshot("logs");
   });
 });
