f8ea664b7d9e3126a19a8d7634bd3bf40a3558ca
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2023-05-08 18:21:04 -0700

    fix: use vanilla `pino` instance for logging (#465)

--- api/github/webhooks.js
+++ api/github/webhooks.js
@@ -1,8 +1,13 @@
 const { createNodeMiddleware, createProbot } = require("probot");
+const pino = require("pino");
 
 const app = require("../../app");
 
 module.exports = createNodeMiddleware(app, {
-  probot: createProbot(),
+  probot: createProbot({
+    overrides: {
+      log: pino(),
+    },
+  }),
   webhooksPath: "/api/github/webhooks",
 });
